eb617f96e5fc5fbf2c17c1e1b55fad80c763b73e
glutix <ferreyra.gluti@gmail.com>
2026-04-21 13:37:02 -0300

    conexion bases de datos

---
+++ config/global.php
@@ -0,0 +1,17 @@
+<?php
+
+define("DB_HOST", "localhost");
+
+define("DB_NAME", "dbsistema");
+
+define("DB_USER", "root");
+
+define("DB_PASSWORD", "asdasd123");
+
+define("DB_ENCODE", "utf8");
+
+define("PRO_NOMBRE", "e-commerce");
+
+define("DB_ZONE", "America/Buenos_Aires");
+
+?>
