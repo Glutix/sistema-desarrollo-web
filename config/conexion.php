20ffbc73d713069109727c98db4ea1ed2aaf04a9
glutix <ferreyra.gluti@gmail.com>
2026-04-21 16:14:54 -0300

    fix conecction database

--- config/conexion.php
+++ config/conexion.php
@@ -1,16 +1,16 @@
 <?php
 require_once "global.php";
 
-$conexion = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME,3306);
+$conexion = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, 3306);
 
-mysqli_query($conexion, "SET NAMES '".DB_ENCODE."'");
-
-
-if (mysqli_connect_errno()) {
-    printf("Fallo la conexion a la base de datos: %s\n", mysqli_connect_error());
-    exit();
+// Verificar conexión (forma correcta OO)
+if ($conexion->connect_error) {
+    die("Fallo la conexión a la base de datos: " . $conexion->connect_error);
 }
 
-echo "Conexion exitosa";
+// Configurar charset correctamente
+$conexion->set_charset(DB_ENCODE);
+
+echo "Conexión exitosa";
 
 ?>
