a445bc75d5a41db125adff3b8dc866698e1a67c6
glutix <ferreyra.gluti@gmail.com>
2026-06-30 02:08:49 -0300

    correccion de sintaxis

--- ajax/articulo.php
+++ ajax/articulo.php
@@ -36,19 +36,16 @@ switch ($_GET["op"]) {
 		$rspta = $articulo->desactivar($idarticulo);
 		echo $rspta ? "Artículo Desactivado" : "Artículo no se puede desactivar";
 		break;
-		break;
 
 	case 'activar':
 		$rspta = $articulo->activar($idarticulo);
 		echo $rspta ? "Artículo activado" : "Artículo no se puede activar";
-		break;
 		break;
 
 	case 'mostrar':
 		$rspta = $articulo->mostrar($idarticulo);
 		//Codificar el resultado utilizando json
 		echo json_encode($rspta);
-		break;
 		break;
 
 	case 'listar':
